b8116720f9ea01cf477a1e9a4548f0c187d65ec8
Beomsoo Kim <jerrybeomsoo@naver.com>
2022-09-27 03:38:16 +0900

    plz

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ function watermark(svg) {
 // Smaller number makes the watermark to sit in more left or above position, for x and y respectively.
   if (watermark_image_url) {
     wm.append("image").attrs({
-      x: 131,
+      x: 133,
       y: 100,
       width: 250,
       height: 68,
